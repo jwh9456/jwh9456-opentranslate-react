b5c31f282197db90f5bc924825b084af1dda53d8
jwh9456 <jwh9456@naver.com>
2023-09-08 16:29:26 +0900

    파일명 state 추가 / mui 정리

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useRef } from 'react';
-import { Input, Button, Fab, TextField } from '@mui/material';
-import { textState, translatedTextState } from './atoms';
+import { Button, Fab, AppBar, Toolbar, useScrollTrigger } from '@mui/material';
+import { textState, translatedTextState, fileState } from './atoms';
 import { useRecoilState } from 'recoil';
 import './App.css';
 
 function App() {
   const [text, setText] = useRecoilState(textState);
   const [translatedText, setTranslatedText] = useRecoilState(translatedTextState);
+  const [file, setFile] = useRecoilState(fileState);
   const [downloadLink, setDownloadLink] = useState(null);
   const fileInputRef = useRef(null);
 
@@ -15,6 +16,7 @@ function App() {
     if (file) {
       const fileText = await file.text();
       const lines = fileText.split('\n');
+      setFile(file);
       setText(fileText);
       setTranslatedText(new Array(lines.length).fill(''));
       setDownloadLink(null);
@@ -46,48 +48,54 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <div>
-            <Fab color='primary' aria-label='add'>
-              <label for="uploadfile">
-                dd
-              </label>
-            </Fab>
-            <Input
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Fab color='primary' aria-label='add'>
+            <label for="uploadfile">
+              dd
+            </label>
+            <input
               id="uploadfile"
               type="file"
               accept=".txt"
               onInput={handleFileInputChange}
               ref={fileInputRef}
             />
-          
-          {text && (
-            <div>
-              {text.split('\n').map((line, i) => (
-                <div key={i}>
-                  {line}
-                  <TextField variant='filled'
-                    value={translatedText[i] || ''}
-                    onChange={(e) => handleTranslatedTextChange(i, e.target.value)}
-                  />
-                </div>
-              ))}
-            </div>
-          )}
-          {text && (
-            <div>
-              <div>Click the button to save text to clipboard</div>
-              <Button variant='contained' onClick={handleExport}>Export File</Button>
-            </div>
-          )}
-          {downloadLink && (
-            <div>
-              <a href={downloadLink} download="translated.txt">
-                Download Translated Text
-              </a>
-            </div>
-          )}
+          </Fab>
+        </Toolbar>
+
+
+
+        {file && <div>File: {file.name}</div>}
+
+        {downloadLink && (
+          <div>
+            <a href={downloadLink} download="translated.txt">
+              Download Translated Text
+            </a>
+          </div>
+        )}
+      </AppBar>
+      <header className="App-header">
+        <div>
+          <div>
+            {text && (
+              <div>
+                {text.split('\n').map((line, i) => (
+                  <div key={i} className='container'>
+                    <span className='line-number'>{i + 1}</span>
+                    <div>{line}</div>
+                    <div><textarea value={translatedText[i] || ''} onChange={(e) => handleTranslatedTextChange(i, e.target.value)} /></div>
+                  </div>
+                ))}
+              </div>
+            )}
+            {text && (
+              <div>
+                <div>Click the button to save text to clipboard</div>
+                <Button variant='contained' onClick={handleExport}>Export File</Button>
+              </div>
+            )}
           </div>
         </div>
       </header >
