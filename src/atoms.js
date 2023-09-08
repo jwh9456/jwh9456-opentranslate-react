ea16f3bf9120d1320478f56d6ec1a80983d034bd
jwh9456 <jwh9456@naver.com>
2023-09-08 16:28:54 +0900

    파일명 state 추가

--- src/atoms.js
+++ src/atoms.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 import { atom } from "recoil";
 
+export const fileState = atom({
+    key: 'file',
+    default: null
+});
+
 export const textState = atom({
     key: 'text',
     default: ''
